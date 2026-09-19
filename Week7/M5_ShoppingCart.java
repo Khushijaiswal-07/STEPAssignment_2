9a246169e7759f8940944c9abeea927f9b708d51
Khushijaiswal-07 <khushibtn@gmail.com>
2026-09-19 16:07:12 +0530

    Completed Week 7 Assignment Problem 5\

---
+++ Week7/M5_ShoppingCart.java
@@ -0,0 +1,50 @@
+class Cart {
+    private double[] prices;
+    private int itemCount;
+    private final String cartId;
+
+    public Cart(String cartId, int maxItems) {
+        this.cartId = cartId;
+        this.prices = new double[maxItems];
+        this.itemCount = 0;
+    }
+
+    public void addItem(double price) {
+        if (itemCount < prices.length && price >= 0) {
+            prices[itemCount] = price;
+            itemCount++;
+        } else if (itemCount >= prices.length) {
+            System.out.println("Cart is full");
+        } else {
+            System.out.println("Price cannot be negative");
+        }
+    }
+
+    public double getTotal() {
+        double total = 0;
+
+        for (int i = 0; i < itemCount; i++) {
+            total = total + prices[i];
+        }
+
+        return total;
+    }
+
+    public int getItemCount() {
+        return itemCount;
+    }
+}
+
+public class M5_ShoppingCart {
+    public static void main(String[] args) {
+
+        Cart cart = new Cart("CART-5", 20);
+
+        cart.addItem(250);
+        cart.addItem(99);
+        cart.addItem(151);
+
+        System.out.println("Total: " + cart.getTotal());
+        System.out.println("Item count: " + cart.getItemCount());
+    }
+}
